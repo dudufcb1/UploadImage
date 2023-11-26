4ab6f9ce065a17210e6daa07ab8376746714f2a6
Eduardo Gonzalez <pctuxtlag@gmail.com>
2023-11-25 22:50:37 -0600

    Agregadas algunas comprobaciones de tamaño, tipo a la subida de imagenes

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,9 +1,17 @@
 const path = require('path');
-const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
+    console.log(req.files)
+    if (!req.files) throw new CustomError.BadRequestError('No files were uploaded.');
+
     let productImage = req.files.image; // imagen
+
+    if (!productImage.mimetype.startsWith('image')) throw new CustomError.BadRequestError('Please upload an image file');
+    const maxSize = 1024 * 1024 * 2; // 2MB
+    if (productImage.size > maxSize) throw new CustomError.BadRequestError(`Please upload an image less than ${maxSize}`);
+
 
     const imagePath = path.join(__dirname, // ruta donde se guardara la imagen
         '../public/uploads/'
